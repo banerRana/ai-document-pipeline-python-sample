4f2765699e166729666aa82617f6174374330fce
Chris Givens <givenscj@hotmail.com>
2025-04-18 12:12:00 -0400

    Update vm_installs.ps1

--- infra/scripts/vm_installs.ps1
+++ infra/scripts/vm_installs.ps1
@@ -2,10 +2,13 @@ $tenantId = 'your-tenant-id'
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+choco install python311 -y
 choco install git -y
 choco install azd -y
 choco install github-desktop -y
-#choco install docker-desktop -y
+choco install powershell-core -y
+
+wsl --update
 
 mkdir C:\github
 cd C:\github
@@ -13,7 +16,17 @@ git clone https://github.com/givenscj/azure-ai-document-pipeline-python-sample
 
 cd .\azure-ai-document-pipeline-python-sample\
 
-az login --use-device-code -tenant $tenantId
+#open visual studio code to the project folder
+code .
+
+az login --use-device-code --tenant $tenantId
 azd auth login --tenant-id $tenantId
 
-#wsl --install
+#copy your .env file to the project folder in the virtual machine
+
+choco install docker-desktop -y
+restart
+
+azd provision --debug
+
+azd up
